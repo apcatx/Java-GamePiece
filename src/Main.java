ea1dd6d236eabe85dcdccb1c0f3661585b0b0208
apcatx <austinpatrickcarey@gmail.com>
2017-08-22 17:08:32 -0400

    GamePiece second commit, finished daily

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,17 @@ public class Main {
     public static void main(String[] args) {
         GamePiece gamePiece = new GamePiece();
         System.out.println(gamePiece.frozen);
+        gamePiece.freeze();
+        System.out.println(gamePiece.frozen);
+        gamePiece.unfreeze();
+        System.out.println(gamePiece.frozen);
+        gamePiece.move(5, 5);
+        System.out.println(gamePiece.positionX);
+        System.out.println(gamePiece.positionY);
+        gamePiece.freeze();
+        System.out.println(gamePiece.frozen);
+        gamePiece.move(6, 6);
+        System.out.println(gamePiece.positionX);
+        System.out.println(gamePiece.positionY);
     }
 }
